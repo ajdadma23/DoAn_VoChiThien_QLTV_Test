a10599d3165780449921ac25f544a52f8c4ac721
Dell <Dell@DESKTOP-DPL3422>
2020-12-04 09:38:49 +0700

    ...

---
+++ src/DTO/book.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author Dell
+ */
+public class book {
+    private String maSach;
+    private String tenSach;
+    private String tacgia;
+    private double giaSach;
+    
+    public book(){};
+    public book(String maSach, String tenSach, String tacgia, double giaSach) {
+        this.maSach = maSach;
+        this.tenSach = tenSach;
+        this.tacgia = tacgia;
+        this.giaSach = giaSach;
+    }
+
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public String getTacgia() {
+        return tacgia;
+    }
+
+    public void setTacgia(String tacgia) {
+        this.tacgia = tacgia;
+    }
+
+    public double getGiaSach() {
+        return giaSach;
+    }
+
+    public void setGiaSach(double giaSach) {
+        this.giaSach = giaSach;
+    }
+    
+}
